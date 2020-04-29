61312ece06e6df4a5b893aef89b7787360ea0a31
capulusnoctis <axel.solares.g@gmail.com>
2020-04-29 00:39:00 -0600

    Added more components!

---
+++ src/components/Profile.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let nickname;
+  export let name;
+</script>
+
+<style>
+  .Profile-content {
+    display: flex;
+    align-items: center;
+  }
+  .Profile-avatar img {
+    width: 42px;
+    height: 42px;
+    border-radius: 50%;
+  }
+  .Profile-info {
+    margin: 0 0 0 0.5em;
+  }
+  .Profile-info h2 {
+    font-size: 14px;
+    color: black;
+    margin: 0;
+    padding: 0;
+  }
+  .Profile-info span {
+    font-size: 12px;
+    font-weight: normal;
+  }
+</style>
+
+<div class="Profile">
+    <div class="Profile-content">
+        <div class="Profile-avatar">
+            <img src="https://lh3.googleusercontent.com/wR7CM3fGLbZM0DEZrbfZ1cR2BEGeUJOxYDQqV7npBILoxMYHeoD5QwFuPeA01qMLXLvrxbvaxWJBlPuKmiaBDjeu4Yl-VgQm8cstHnF8FgUnP8NHP7DiK0h1e3Ad2dwnKfNscMhhodKl6k6jFCOtdozNtEmglis3YOildZCNkQuKW06sUP-pL_nBxOiSrc9N0w4L8DV0lM15y4Hu49SU-scZmuDJDBwewvKP6vYZAuBqn85odObECrg99_HOrTJbE_vyu_TZpF_-jcW4qJjgMsSzCvMUQUNOGyJ_dp0YpPpeYLArkMbT34NyHZk7IySIzrlnD8hVR5bRO88D35QQ71n_rWKrbXbTPP7N--bd6G7dPub4Oy1WYA90xHzqapNKqwo_dyDbUQ-YxXTZ-58_TSShT0aOVSi8nph07qXHBvH9m0pYoxDs3781yjnI_8rt26yCPGlb-ufCGsE943OqmmXPYh8fRvRsrOmhdC58Xx1AtUykuON4FFYkl7YmsgEQ6Wepx6jbS4q6jv1pLBO_wgztr0ibXDN0MLy8XsqsFhKe690JIa7vi1IwihQykGD4fLLJ1No-N1fYFwZD2HahrWgMd2hHb_RjdoK9FvOkEEgIifSl8uetUAcZ9vTYXbuUa9YFBBVN90TlxTqA0bFEg-ryMdnglDO3C_J_Q2g-ZHQL1ZFcTEue470b7c6d=s635-no" alt="Profile" />
+        </div>
+        <div class="Profile-info">
+            <h2>axeljsolares</h2>
+            <span>Axel J. Solares</span>
+        </div>
+    </div>
+</div>
---
+++ src/components/Stories.svelte
@@ -0,0 +1,78 @@
+<style>
+  .Stories {
+    border: 1px solid rgba(219, 219, 219, 1);
+    border-radius: 4px;
+    margin: 1em 0;
+    padding: 0.5em 1em;
+    background-color: white;
+  }
+  .Stories-item img {
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+  }
+  .Stories-item {
+    display: flex;
+    align-items: center;
+    margin: 0 0 0.5em 0;
+  }
+  .Stories-item h2 {
+    font-size: 14px;
+    color: black;
+    margin: 0 0 0 0.5em;
+  }
+  .Stories-item h2 span {
+    display: block;
+    font-size: 9px;
+    text-transform: uppercase;
+    color: gray;
+  }
+  .Stories-head {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5em 0 0 0;
+  }
+  .Stories-head h2 {
+    font-size: 12px;
+    font-weight: 600;
+  }
+  .Stories-head span {
+    font-size: 12px;
+    font-weight: normal;
+    color: black;
+  }
+  .Stories-items {
+    margin: 0.5em 0 0.5em 0;
+  }
+  .Stories-item-box {
+    width: 32px;
+    height: 32px;
+    border: double 2px transparent;
+    border-radius: 100%;
+    background-image: linear-gradient(#fff, #fff),
+      radial-gradient(circle at top left, #f09433, #bc1888);
+    background-origin: border-box;
+    background-clip: content-box, border-box;
+  }
+</style>
+
+<div class="Stories">
+    <div class="Stories-container">
+        <div class="Stories-head">
+            <h2>Historias</h2>
+            <span>Ver todas</span>
+        </div>
+        <div class="Stories-items">
+            <div class="Stories-item">
+                <div class="Stories-item-box">
+                    <img src="https://lh3.googleusercontent.com/wR7CM3fGLbZM0DEZrbfZ1cR2BEGeUJOxYDQqV7npBILoxMYHeoD5QwFuPeA01qMLXLvrxbvaxWJBlPuKmiaBDjeu4Yl-VgQm8cstHnF8FgUnP8NHP7DiK0h1e3Ad2dwnKfNscMhhodKl6k6jFCOtdozNtEmglis3YOildZCNkQuKW06sUP-pL_nBxOiSrc9N0w4L8DV0lM15y4Hu49SU-scZmuDJDBwewvKP6vYZAuBqn85odObECrg99_HOrTJbE_vyu_TZpF_-jcW4qJjgMsSzCvMUQUNOGyJ_dp0YpPpeYLArkMbT34NyHZk7IySIzrlnD8hVR5bRO88D35QQ71n_rWKrbXbTPP7N--bd6G7dPub4Oy1WYA90xHzqapNKqwo_dyDbUQ-YxXTZ-58_TSShT0aOVSi8nph07qXHBvH9m0pYoxDs3781yjnI_8rt26yCPGlb-ufCGsE943OqmmXPYh8fRvRsrOmhdC58Xx1AtUykuON4FFYkl7YmsgEQ6Wepx6jbS4q6jv1pLBO_wgztr0ibXDN0MLy8XsqsFhKe690JIa7vi1IwihQykGD4fLLJ1No-N1fYFwZD2HahrWgMd2hHb_RjdoK9FvOkEEgIifSl8uetUAcZ9vTYXbuUa9YFBBVN90TlxTqA0bFEg-ryMdnglDO3C_J_Q2g-ZHQL1ZFcTEue470b7c6d=s635-no" alt="Profpic">
+                </div>
+                <h2>
+                    axeljsolares
+                    <span>10 horas antes</span>
+                </h2>
+            </div>
+        </div>
+    </div>
+</div>
