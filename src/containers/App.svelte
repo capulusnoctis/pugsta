e748369283f730b2b6f3db5d2593f21e941458b0
capulusnoctis <axel.solares.g@gmail.com>
2020-09-15 09:45:14 -0600

    Changed style on App container

--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -15,10 +15,9 @@
 </script>
 
 <style>
-    :global(body) {
-        @import url("https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap");
-        @import url("https://fonts.googleapis.com/css2?family=Pacifico&display=swap");
-        
+    @import url("https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap");
+    @import url("https://fonts.googleapis.com/css2?family=Pacifico&display=swap");
+    :global(body) {        
         background-color: #fafafa;
         color: rgba(38,38,38,0.7);
         font-family: "Lato", sans-serif;
